46cc5c6e53ac8fd1bbca64c0d94ee78ee3a5b0b3
prasad <botreprasad474@gmail.com>
2025-01-16 09:21:02 +0530

    finasl commit

--- Main.java
+++ Main.java
@@ -2,46 +2,28 @@ import java.util.*;
 
 class Main {
 
-public static int binarysearch(int[] arr, int key){
-
-int start = 0 ;
-int end = arr.length -1;
-
-while(start<=end){
-int mid = (start + end)/2;
-
-if(arr[mid] == key){
-
-    return mid;
-}
-else if(arr[mid] > key){
+public static int binarysearch(){
 
 
-    end  = mid -1 ;
-}else{
 
 
-    start = mid +1;
+
+    HashMap<String, Integer >map = new HashMap<>();
+
+
+    map.put("First", 1);
+    map.put("Second", 2);
+    map.put("Third", 3);
+
+for(String key : map.keySet()){
+
+
+    
 }
 
 
 
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-return 0;
+return 1;
 }
 
     public static void main(String[] args) {
@@ -49,7 +31,7 @@ return 0;
 
   int [] arr = {1,2,3,4,5,6,7,8};
   int key = 3;
-System.out.println(binarysearch(arr, key));
+System.out.println(binarysearch());
 
 
 
--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,29 +3,7 @@
 using namespace std;
 
 
-void solve(vector<string> str, int n ){
-
-pair<int ,string>p1;
-   priority_queue<pair<int, string>, vector<pair<int, string>>
-for(auto i : str){
-str temp = "";
-int num ;
-for(auto j :i){
-
-if(isalpha(j)){
-    temp+=j;
-}
-else{
-num = stoi(j);
-}
-
-}
-
-p1=make_pair(num ,temp);
-
-
-
-}
+void BubblrSort(){
 
 
 
