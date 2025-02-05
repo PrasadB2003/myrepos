442f92bda82f6c8f7e7046194c1568ef55a50aa6
prasad <botreprasad474@gmail.com>
2025-02-05 23:10:31 +0530

    commited

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -4,12 +4,14 @@ class Node {
 public:
 
 int data ;
- Node * next ;
-
+ Node * left ;
+Node* right;
 
  Node(int d){
 data = d ;
-next = nullptr;
+left = nullptr;
+right = nullptr;
+
 
 
  }
@@ -17,43 +19,26 @@ next = nullptr;
 };
 
 
-class LinkledList{
+class binaryTree{
+private :
 
-private:
-Node * head ;
-Node * Tail;
+Node * root;
 
 public:
 
+binaryTree(){
+Node * root = nullptr;
 
-LinkledList(){
-
-
-    head = nullptr;
-    Tail = nullptr;
 }
 
-
-void insert(int data){
-
-Node * NewNode = new Node(data);
+void insert(Node *node , int data){
 
 
-if(head == nullptr){
 
-    head = NewNode;
-}
-else{
-Node * temp = head;
 
-while(temp->next != nullptr){
 
-    temp = temp->next;
-}
 
-temp->next = NewNode;
-Tail = NewNode;
-}
+
 
 
 
@@ -61,67 +46,7 @@ Tail = NewNode;
 }
 
 
-void display(){
-Node * temp = head ;
 
-while(temp !=  nullptr){
-
-   
- 
-     cout<<temp->data<<"-> ";
-        temp = temp->next;
-}
-
-cout<<endl;
-
-
-}
-
-void insertAtpos(int pos , int data){
-
-Node * prev = head ;
-Node * prev1 = nullptr;
-while(pos != 1){
-prev1 = prev;
-
-prev = prev->next;
-
-
-pos--;
-
-
-}
-
-Node * NewNode = new Node(data);
-
-
-prev1->next = NewNode;
-NewNode->next = prev;
-
-}
-
-
-void deleteatpos(int pos){
-
-Node * prev = head ;
-Node * prev1 = nullptr;
-while(pos != 1){
-prev1 = prev;
-
-prev = prev->next;
-
-
-pos--;
-
-
-}
-
-Node * temp = prev;
-
-prev1->next = prev->next;
-
-delete temp;
-}
 
 
 
@@ -129,22 +54,8 @@ delete temp;
 
 };
 
-
-
 int main(){
 
 
-LinkledList L ;
 
-L.insert(10);
-
-L.insert(20);
-L.insert(30);
-L.insert(40);
-
-
-
-L.display();
-L.deleteatpos(3);
-L.display();
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,20 @@
-
-
-void insertionSort(vector<int>&arr , in
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2;
+    
+   
+    do {
+        cout << "Enter first number: ";
+        cin >> num1;
+        
+        do {
+            cout << "Enter second number: ";
+           cin >> num2;
+        } while (num2 != 0);
+        
+        cout << "Sum = " << num1 + num2 << std::endl;
+    } while (num1 == 0 || num2 == 0);
+    
+    return 0;
+}
